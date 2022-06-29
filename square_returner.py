492a986a7d4a04444bd41951f124496845079ca3
aphxtwin <99614711+aphxtwin@users.noreply.github.com>
2022-06-29 16:53:14 -0300

    Update square_returner.py

--- square_returner.py
+++ square_returner.py
@@ -4,14 +4,14 @@ Write a short Python function that takes a positive integer n and returns
 the sum of the squares of all the positive integers smaller than n.
 """
 def n(k):
-    if k > 0:
-        listi= []
-        while k > 0:
-            k = k-1
-            listi.append(k)
-        
-        result=0
-    
-        for i in listi:
-            result= result+i
-        print(result)
+
+    listi= []
+    while k > 0:
+        k = k-1
+        listi.append(k)
+
+    result=0
+
+    for i in listi:
+        result= result+i**2
+    print(result)
