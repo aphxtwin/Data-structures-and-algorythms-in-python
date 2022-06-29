5ac7ce434b529c9f324a26999c42979ae6370141
aphxtwin <99614711+aphxtwin@users.noreply.github.com>
2022-06-29 16:55:43 -0300

    Update square_returner.py

--- square_returner.py
+++ square_returner.py
@@ -15,3 +15,21 @@ def n(k):
     for i in listi:
         result= result+i**2
     print(result)
+
+"""
+Write a short Python function that takes a positive integer n and returns
+the sum of the squares of all the odd positive integers smaller than n.
+"""
+def n(k):
+    if k > 0:
+        listi= []
+        while k > 0:
+            k = k-1
+            if k%2 != 0:
+                listi.append(k)
+        
+        result=0
+    
+        for i in listi:
+            result= result+ i**2
+        print(result)
